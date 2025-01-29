56ef2d6963bac16b86473e02f328bec241ee121c
Jan Bachmann <jan@mannbach.de>
2025-01-29 15:59:03 -0700

    Make README more verbose

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
 # template_python
-
-## Description
-A new Python project.
+To use this template, execute the scripts provided in the 'scripts/setup' folder in the order they are numbered (see [Setup](#setup)).
+This will create a new Python project with a custom name and Python version.
+The result will be identical to the structure of this project (see [Content](#content) for a detailed description).
+Every  to the remote repository will trigger a linting (all code) and testing process (code in ).
 
 ## Setup
-Instructions to set up the project.
+Run the following scripts:
+01. `./scripts/setup/01_setup_project.sh <project_name> <python_version>` - Create a new Python project with a custom name and Python version, altering the current project files.
+02. `./scripts/setup/02_setup_build_docker.sh` - Build the Docker image and installs the package therein.
+03. `./scripts/setup/03_setup_run_docker.sh` - Run the Docker container and keep it running. This script might need to be run again after a restart.
 
 ## Usage
-Instructions to use the project.
+Run `./scripts/setup/docker_exec.sh <command>` to run a command inside the Docker container and `./scripts/setup/bump_version.sh` to increment the package version.
 
 ## Content
-Description of files and folders.
+```bash
+├── Dockerfile # Dockerfile for the project
+├── README.md # This readme
+├── config # Configuration files. Default will be config/.env
+│   └── sample.env # Sample environment file
+├── data # Data files
+├── docker-compose.yml # Docker compose configuration
+├── output # Output files
+├── pyproject.toml # Package configuration file
+├── requirements.txt # Python package requirements
+├── scripts # Scripts to be executed from the command line
+│   └── setup # Project folder setup scripts
+├── sketches # Folder to contain scripts that are not tracked by git
+├── template_python # Source code folder
+│   ├── __init__.py # Package __init__ file containing the version
+└── tests # Test scripts folder
+    └── test_load_package.py # Test script to check if package loads
+```
--- scripts/setup/01_setup_project.sh
+++ scripts/setup/01_setup_project.sh
@@ -25,18 +25,39 @@ EOL
 echo "Creating 'README.me'."
 cat <<EOL > README.md
 # $PROJECT_NAME
-
-## Description
-A new Python project.
+To use this template, execute the scripts provided in the 'scripts/setup' folder in the order they are numbered (see [Setup](#setup)).
+This will create a new Python project with a custom name and Python version.
+The result will be identical to the structure of this project (see [Content](#content) for a detailed description).
+Every `git push` to the remote repository will trigger a linting (all code) and testing process (code in `tests/`).
 
 ## Setup
-Instructions to set up the project.
+Run the following scripts:
+01. \`./scripts/setup/01_setup_project.sh <project_name> <python_version>\` - Create a new Python project with a custom name and Python version, altering the current project files.
+02. \`./scripts/setup/02_setup_build_docker.sh\` - Build the Docker image and installs the package therein.
+03. \`./scripts/setup/03_setup_run_docker.sh\` - Run the Docker container and keep it running. This script might need to be run again after a restart.
 
 ## Usage
-Instructions to use the project.
+Run \`./scripts/setup/docker_exec.sh <command>\` to run a command inside the Docker container and \`./scripts/setup/bump_version.sh\` to increment the package version.
 
 ## Content
-Description of files and folders.
+\`\`\`bash
+├── Dockerfile # Dockerfile for the project
+├── README.md # This readme
+├── config # Configuration files. Default will be config/.env
+│   └── sample.env # Sample environment file
+├── data # Data files
+├── docker-compose.yml # Docker compose configuration
+├── output # Output files
+├── pyproject.toml # Package configuration file
+├── requirements.txt # Python package requirements
+├── scripts # Scripts to be executed from the command line
+│   └── setup # Project folder setup scripts
+├── sketches # Folder to contain scripts that are not tracked by git
+├── $PROJECT_NAME # Source code folder
+│   ├── __init__.py # Package __init__ file containing the version
+└── tests # Test scripts folder
+    └── test_load_package.py # Test script to check if package loads
+\`\`\`
 EOL
 
 # Create pyproject.toml
