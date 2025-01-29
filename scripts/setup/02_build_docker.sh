f7820aee002eaba6b267cf6fd3db8e4a6c6734b3
Jan Bachmann <jan@mannbach.de>
2025-01-29 11:38:11 -0700

    Fix path to sample.env

--- scripts/setup/02_build_docker.sh
+++ scripts/setup/02_build_docker.sh
@@ -2,12 +2,13 @@
 
 # Check if optional environment file is provided and source it if it is.
 # Otherwise, source from `../config/.env`.`
-if [ "$#" -eq 1 ]; then
-    echo "Sourcing environment file: $1"
-    ENV_FILE=$1
-else
-    echo "Sourcing environment file: config/.env"
-    ENV_FILE="config/.env"
+# Default .env file path
+ENV_FILE="config/.env"
+
+# Check if the first argument is --env-file
+if [[ "$1" == "--env-file" ]]; then
+    ENV_FILE="$2"
+    shift 2
 fi
 
 # Build Docker image
