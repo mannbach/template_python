b3d50d37b4888856041f723d664bbef53ed077b7
Jan Bachmann <jan@mannbach.de>
2025-01-28 17:53:44 -0700

    Script to build docker image

---
+++ scripts/setup/02_build_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if optional environment file is provided and source it if it is.
+# Otherwise, source from `../config/.env`.`
+if [ "$#" -eq 1 ]; then
+    echo "Sourcing environment file: $1"
+    ENV_FILE=$1
+else
+    echo "Sourcing environment file: ../../config/.env"
+    ENV_FILE="../../config/.env"
+fi
+
+# Build Docker image
+echo "Building Docker image."
+docker-compose --env-file $ENV_FILE build
